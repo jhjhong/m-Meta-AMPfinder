5ae9a322213bc103360222b793aed7c81648c5ca
jhjhong <jhihua.tw@gmail.com>
2022-12-20 10:12:56 +0800

    function load

--- script/load.py
+++ script/load.py
@@ -2,30 +2,44 @@ import shutil
 import argparse
 from script.settings import *
 
-def write_fasta_from_json(args):
-	# working_directory = os.getcwd()
-	# if os.path.isfile(os.path.join(self.db, "proteindb.fsa")):
-	# 		# logger.info("Database already exists.")
-	# 		return
-	# else:
-		working_directory = os.getcwd()
-		with open(os.path.join(working_directory, args.input_json), 'r') as jfile:
+def write_fasta_from_json(input_json):
+	# Creates a fasta file from json file.
+	if os.path.isfile(os.path.join(data_path, "proteindb.fsa")):
+		logger.info("Database already exists.")
+		return
+	else:
+		if not os.path.exists(db_path):
+			logger.info("Making database folder at {}".format(PATH))
+			os.system('mkdir ' + db_path)
+		if not os.path.exists(path):
+			logger.info("Making database folder {}".format(path))
+			os.system('mkdir ' + path)
+		if not os.path.exists(data_path):
+			logger.info("Making datafolder {}".format(data_path))
+			os.system('mkdir ' + data_path)
+
+		with open(os.path.join(PATH, input_json), 'r') as jfile:
 			data = json.load(jfile)
-			with open(os.path.join(working_directory, "database.fasta"), 'a') as fout:
+			with open(os.path.join(data_path, "proteindb.fsa"), 'a') as fout:
 				for i in range(0, len(data)):
 					header = (data[i]['dbAMP_ID'])
 					sequence = data[i]['Seq']
 					fout.write(">{}\n".format(header))
 					fout.write("{}\n".format(sequence))
-		print("make db finish")
+		logger.info("Database extract finish: {}".format(os.path.join(data_path, "proteindb.fsa")))
+
 
 def main(args):
-	write_fasta_from_json(args)
+	if args.debug:
+		logger.setLevel(10)
+
+	write_fasta_from_json(args.input_json)
 
 
 def create_parser():
 	parser = argparse.ArgumentParser(prog="macpfinder load", description="{} - {} - Load".format(APP_NAME, SOFTWARE_VERSION))
 	parser.add_argument('-i', '--input_json', required=True, help="input file must be in json format! e.g ampfinder_210729.json")
+	parser.add_argument('--debug', dest="debug", action="store_true", help="debug mode")
 	return parser
 
 def run():
