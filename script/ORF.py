3c4c027f611ae7fd4004557e7012bd277b0d8574
jhjhong <jhihua.tw@gmail.com>
2023-03-01 16:44:34 +0800

    filter orf length

--- script/ORF.py
+++ script/ORF.py
@@ -1,6 +1,7 @@
 
 import logging
 import tempfile, time, fileinput, math, multiprocessing, shutil
+from Bio import SeqIO
 from script.settings import *
 
 class PyORF(object):
@@ -14,30 +15,54 @@ class PyORF(object):
     def __repr__(self):
         return "ORF({}".format(self.__dict__)
 
-    # def min_max_sequence_length(self):
-    # 	sequences = []
-    # 	for record in SeqIO.parse(self.input_file, "fasta"):
-    # 		sequences.append(len(record.seq))
-    # 	return min(sequences), max(sequences), len(sequences)
+    def min_max_sequence_length(self):
+    	sequences = []
+    	for record in SeqIO.parse(self.input_file, "fasta"):
+    		sequences.append(len(record.seq))
+    	return min(sequences), max(sequences), len(sequences)
+
+    def filter_smorfs(self):
+        long_sequences = []
+
+        cmd = "sed 's/*//g' {input_file} > {output_file}" \
+        .format(
+                input_file = os.path.join(self.output_dir, "temp.smorfs/temp.contig.fsa"),
+                output_file = os.path.join(self.output_dir, "temp.smorfs/temp.contig.fsa1")
+            )
+        # print(cmd)
+        os.system(cmd)
+
+        input_file = os.path.join(self.output_dir, "temp.smorfs/temp.contig.fsa1")
+        output_file = os.path.join(self.output_dir, "temp.smorfs/temp.contig.L100.fsa")
+        output_handle = open(output_file, "w")
+
+        try:
+            for record in SeqIO.parse(input_file, "fasta") :
+                if len(record.seq) >= 10 and len(record.seq) <= 100 :
+                    # Add this record to our list
+                    long_sequences.append(record)
+
+            SeqIO.write(long_sequences, output_file, "fasta")
+            logger.info("filter done")
+            
+        except Exception as e:
+            logger.exception("failed to write orf file")
+        else:
+            pass
 
     def run(self):
         logger.info("predict ORF")
-
-        # check quality if it need meta mode (low_quality).
-        records = list(SeqIO.parse(self.input_file, "fasta"))
-        sequences = [ str(record.seq) for record in records ]
-        minimum_sequence_length = min((len(seq) for seq in sequences), default=0)
-        # minimum_sequence_length, maximum_sequence_length, number_of_sequences = self.min_max_sequence_length()
+        minimum_sequence_length, maximum_sequence_length, number_of_sequences = self.min_max_sequence_length()
 
         quality = "-n -p single"
         if self.low_quality == True or minimum_sequence_length < 20000:
             quality = "-p meta"
 
         filename = os.path.basename(self.input_file)
-        trans_file="{}.temp.contig.fsa".format(filename)
-        output_file="{}.temp.draft".format(filename)
-        nuc_file="{}.temp.contigToORF.fsa".format(filename)
-        potential_genes="{}.temp.potentialGenes".format(filename)
+        trans_file = "temp.smorfs/temp.contig.fsa"
+        output_file = "temp.smorfs/temp.draft"
+        nuc_file = "temp.smorfs/temp.contigToORF.fsa"
+        potential_genes = "temp.smorfs/temp.potentialGenes"
 
         if not os.path.exists(self.output_dir):
             logger.info("Making temp.orf folder {}".format(self.output_dir))
@@ -53,14 +78,26 @@ class PyORF(object):
                 quality=quality,
                 stdout=stdout,
                 nuc_file=os.path.join(self.output_dir, nuc_file),
-                potential_genes=os.path.join(self.output_dir, potential_genes),
+                potential_genes=os.path.join(self.output_dir, potential_genes)
             )
 
-        print(cmd)
+        logger.info(cmd)
         os.system(cmd)
+
+        # filter 10 <= sequence length <= 100 for next classifier prediction
+        self.filter_smorfs()
+
 
         if self.clean == True and os.path.exists(os.path.join(self.output_dir, output_file)):
             os.remove("{output_file}".format(output_file=os.path.join(self.output_dir, output_file)))
+
+        # move final file
+        original = os.path.join(self.output_dir, "temp.smorfs/temp.contig.fsa")
+        original_L100 = os.path.join(self.output_dir, "temp.smorfs/temp.contig.L100.fsa")
+        target = os.path.join(self.output_dir, "final.smorfs.fsa")
+        target_L100 = os.path.join(self.output_dir, "final.smorfs.L100.fsa")
+        os.system("ln -s {final} {soft_link}".format(final = original, soft_link = target))
+        os.system("ln -s {final} {soft_link}".format(final = original_L100, soft_link = target_L100))
 
 
 
