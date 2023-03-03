4075250876c4775ab22edc9a26013d6f9a9115d9
jhjhong <jhihua.tw@gmail.com>
2023-03-03 13:36:10 +0800

    modified logger

--- script/settings.py
+++ script/settings.py
@@ -29,14 +29,14 @@ data_path = os.path.join(db_path, "_data/")
 # LOGGING CONFIG
 # ====================================================================================
 # https://editor.leonh.space/2022/python-log/ logging 找時間回來改
-level = logging.WARNING
+level = logging.INFO
 logger = logging.getLogger(__name__)
 logger.setLevel(level)
 
 # detailed log
-# formatter = logging.Formatter('%(levelname)s %(asctime)s : (%(filename)s::%(funcName)s::%(lineno)d) : %(message)s')
-# basic log
 formatter = logging.Formatter('%(levelname)s %(asctime)s : (%(filename)s::%(funcName)s::%(lineno)d) : %(message)s')
+# basic log
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s %(message)s')
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
@@ -47,7 +47,7 @@ logger.addHandler(stream_handler)
 # APP INFO
 # ====================================================================================
 APP_NAME="m(Meta)ACPfinder"
-SOFTWARE_VERSION = "v0.0.1"
+SOFTWARE_VERSION = "v0.2.0"
 SOFTWARE_SUMMARY = 'Use the m(Meta)ACPfinder to predict candidate ACP from protein or nucleotide'
 
 PATH = os.getcwd()
